d798f094747929e172fe0bfa211d1070aa0c10b8
user <just_beta@hotmail.com>
2018-01-18 06:28:54 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SvetCakesIOS
+
+An webview ios app that show the following website http://svetcakes.me/ have option to call the business number
---
+++ SvetCakesIOS/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//The Follwing Class:
+//* on loadView() create WKWebView
+//* on viewDidLoad() set U
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate{
+    
+    var webView: WKWebView!
+    
+    override func loadView() {
+        //create new web view configuration
+        let webConfiguration = WKWebViewConfiguration()
+        //internalising web view
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        //sets delegate
+        webView.navigationDelegate = self
+        //set the current view to webview
+        view = webView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //set the urls of the website
+        let url = URL(string: "https://bissextile-basket.000webhostapp.com")!
+        //loads the url to webview
+        webView.load(URLRequest(url: url))
+        //alows option to swipe left and right
+        webView.allowsBackForwardNavigationGestures = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
